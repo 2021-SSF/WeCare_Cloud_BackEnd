96ac6155130b4534ac95df6ec1cbe43d4354f04b
MinSeo Cho <chj2171@gmail.com>
2021-11-23 17:29:56 +0900

    FEAT : kakao talk connect and make api for DB

--- wecare/facility/views.py
+++ wecare/facility/views.py
@@ -8,9 +8,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from facility.models import Room, Elder, ElderStatus, Violence
-from facility.serializer import RoomSerializer, ElderSerializer, ElderStatusSerializer, ViolenceSerializer
+from facility.models import Room, Elder, ElderStatus, Violence,Bed
+from facility.serializer import RoomSerializer, ElderSerializer, ElderStatusSerializer, ViolenceSerializer,BedSerializer
 
+from facility.kakao import send_talk
 
 @api_view(['GET'])
 def facility_room_list(request):  # 병원 정보 페이지 리스트
@@ -35,7 +36,7 @@ def elder_detail(request, elder_id):  # 환자 상세정보
 
 @api_view(['POST'])
 def elder_status_create(request):
-    serializer = ElderStatusSerializer(request.data)
+    serializer = ElderStatusSerializer(data=request.data)
 
     if serializer.is_valid():
         serializer.save()
@@ -43,15 +44,39 @@ def elder_status_create(request):
 
     else:
         return Response({"message": "failed"})
+
+
+@api_view(['GET'])
+def elder_status_list(request, elder_id):
+    elder_status = ElderStatus.objects.get(elder_id=elder_id)
+
+    serializer = ElderStatusSerializer(elder_status,many=True)
+    print(serializer.data)
+    return Response(serializer.data)
 
 
 @api_view(['POST'])
 def incident_create(request):
-    serializer = ViolenceSerializer(request.data)
-
+    send_talk() #폭행 발생 카카오톡 보내기
+    serializer = ViolenceSerializer(data=request.data)
+    
     if serializer.is_valid():
         serializer.save()
         return Response({"message": "success"})
 
     else:
         return Response({"message": "failed"})
+
+
+@api_view(['GET'])
+def incident_list(request, room_id):
+    bed = Bed.objects.filter(room_id=room_id)
+
+    violence = Violence.objects.filter(bed_id_id__in=bed)
+    serializer = ViolenceSerializer(violence, many=True)
+    return Response(serializer.data)
+
+
+
+
+
